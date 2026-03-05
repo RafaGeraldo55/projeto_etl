4a48dc09d5149f798df995d1df1629bcbf1f1ef2
Rafa_Geraldo <rafael_geraldo@outlook.com>
2026-03-05 17:11:47 -0300

    Add o load dos dados em .jason

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,24 +1,38 @@
+from json import load
 import requests
-print('Nova versao do projeto ETL')
+from tinydb import TinyDB
+import time
+
 
 def extrai_dados_bitcoin():
     url = 'https://api.coinbase.com/v2/prices/BTC-USD/spot'
     response = requests.get(url)
-    dados = response.json()
-    return dados
-
+    return(response.json())
+    
 def transforma_dados_bitcoin(dados):
     cripto = dados['data']['base']
     valor_bitcoin = dados['data']['amount']
     moeda = dados['data']['currency']
-    return {
+    dados_transformados = {
         'cripto': cripto,
         'valor': valor_bitcoin,
         'moeda': moeda
     }
+    return dados_transformados
+
+def load(dados_transformados):
+        db = TinyDB('db.json')
+        db.insert(dados_transformados)
+        print('Dados carregados com sucesso')
+
 
 if __name__ == '__main__':
-    dados = extrai_dados_bitcoin()
-    dados_transformados = transforma_dados_bitcoin(dados)
-    print(dados)
-    print(dados_transformados)
+    while True: 
+        try:
+            dados = extrai_dados_bitcoin()
+            dados_transformados = transforma_dados_bitcoin(dados)
+            load(dados_transformados)
+            time.sleep(5)
+        except Exception as e:
+            print(f'Erro: {e}')
+            time.sleep(5)
